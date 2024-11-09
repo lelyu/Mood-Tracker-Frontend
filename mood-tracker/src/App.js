b7a3c8cfa202fe1d6e3b94cbd07ebe05281686d5
Lok Leoi <lyulelok@gmail.com>
2024-11-08 22:54:10 -0500

    isLoggedIn API

--- mood-tracker/src/App.js
+++ mood-tracker/src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
@@ -7,21 +7,40 @@ import MoodForm from './components/Mood/MoodForm'
 import MoodView from './components/Mood/MoodView'
 import './css/Nav.css'
 import Dashboard from './components/Dashboard/Dashboard'
+import axios from 'axios'
 
-const App = () => (
-	<Router>
-		<nav className='navbar'>
-			<Link to='/'>Mood Tracker App</Link>
-			<Link to='/login'>Login</Link>
-			<Link to='/register'>Register</Link>
-		</nav>
-		<Routes>
-			<Route path='/login' element={<Login />} />
-			<Route path='/register' element={<Register />} />
-			<Route path='/' element={<Dashboard />} />
-			<Route path='/mood' element={<MoodView />} />
-		</Routes>
-	</Router>
-)
+const App = () => {
+	const API_URL = 'http://localhost:3000/api/v1/'
+	const [isAuthenticated, setIsAuthenticated] = useState(false)
+	// call isLoggedIn API from backend to check whether the user is logged in or not
+	useEffect(() => {
+		const checkLoggedIn = async () => {
+			try {
+				const response = await axios.get(API_URL + 'isloggedin', {
+					withCredentials: true,
+				})
+				setIsAuthenticated(response.data.isLoggedIn)
+			} catch (err) {
+				console.error(err)
+			}
+		}
+		checkLoggedIn()
+	}, [])
 
+	return (
+		<Router>
+			<nav className='navbar'>
+				<Link to='/'>Mood Tracker App</Link>
+				{isAuthenticated && <Link to='/mood'>Mood</Link>}
+				<Link to='/register'>Register</Link>
+			</nav>
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/' element={<Dashboard />} />
+				<Route path='/mood' element={<MoodView />} />
+			</Routes>
+		</Router>
+	)
+}
 export default App
