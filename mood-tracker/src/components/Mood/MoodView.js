3b4efd6ab2b133ca2aa080b41f5b98b4b3ac5399
Lok Leoi <lyulelok@gmail.com>
2024-11-25 13:32:11 -0500

    multiple delete

--- mood-tracker/src/components/Mood/MoodView.js
+++ mood-tracker/src/components/Mood/MoodView.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import '../../css/MoodView.css'
+
 const MoodView = () => {
 	const [moods, setMoods] = useState([])
 	const [error, setError] = useState('')
@@ -10,6 +11,7 @@ const MoodView = () => {
 		intensity: '',
 		note: '',
 	})
+	const [selectedMoods, setSelectedMoods] = useState([])
 	const API_URL = 'http://localhost:3000/api/v1/mood/'
 
 	useEffect(() => {
@@ -39,6 +41,25 @@ const MoodView = () => {
 		}
 	}
 
+	const deleteSelectedMoods = async () => {
+		try {
+			await axios.post(
+				API_URL + 'delete-multiple',
+				{ ids: selectedMoods },
+				{
+					withCredentials: true,
+				}
+			)
+			setMoods((prevMoods) =>
+				prevMoods.filter((mood) => !selectedMoods.includes(mood._id))
+			)
+			setSelectedMoods([])
+			setError('')
+		} catch (err) {
+			setError(err.response?.data?.error || 'An error occurred')
+		}
+	}
+
 	const startEditing = (mood) => {
 		setEditingId(mood._id)
 		setEditData({
@@ -47,6 +68,7 @@ const MoodView = () => {
 			note: mood.note,
 		})
 	}
+
 	const validateEditData = () => {
 		const { intensity, note } = editData
 		if (intensity < 1 || intensity > 10) {
@@ -86,100 +108,131 @@ const MoodView = () => {
 		}
 	}
 
+	const handleCheckboxChange = (id) => {
+		setSelectedMoods((prevSelected) =>
+			prevSelected.includes(id)
+				? prevSelected.filter((moodId) => moodId !== id)
+				: [...prevSelected, id]
+		)
+	}
+
 	return (
 		<div className='mood-view'>
 			<h1>Mood Tracker</h1>
+			<div className='delete-selected-button'>
+				<button className='' onClick={deleteSelectedMoods}>
+					Delete Selected
+				</button>
+			</div>
 			{error && <div className='error'>{error}</div>}
 			{moods.length === 0 ? (
 				<div>No moods to display</div>
 			) : (
-				<ul>
-					{moods.map((mood) => (
-						<li key={mood._id}>
-							<div>
-								<strong>Mood:</strong> {mood.mood}
-							</div>
-							<div>
-								<strong>Intensity:</strong> {mood.intensity}
-							</div>
-
-							<div>
-								<strong>Timestamp:</strong>{' '}
-								{new Date(mood.createdAt).toLocaleString()}
-							</div>
-
-							<div>
-								<strong>Note:</strong> {mood.note}
-							</div>
-
-							{editingId === mood._id ? (
+				<>
+					<ul>
+						{moods.map((mood) => (
+							<li key={mood._id}>
+								<input
+									className='select-checkbox'
+									type='checkbox'
+									checked={selectedMoods.includes(mood._id)}
+									onChange={() =>
+										handleCheckboxChange(mood._id)
+									}
+								/>
 								<div>
-									<select
-										type='text'
-										placeholder='Mood'
-										value={editData.mood}
-										onChange={(e) =>
-											setEditData({
-												...editData,
-												mood: e.target.value,
-											})
-										}>
-										<option value=''>Select Mood</option>
-										<option value='happy'>Happy</option>
-										<option value='sad'>Sad</option>
-										<option value='angry'>Angry</option>
-										<option value='relaxed'>Relaxed</option>
-										<option value='stressed'>
-											Stressed
-										</option>
-									</select>
-									<input
-										type='text'
-										placeholder='Intensity'
-										value={editData.intensity}
-										onChange={(e) =>
-											setEditData({
-												...editData,
-												intensity: e.target.value,
-											})
-										}
-									/>
-									<textarea
-										type='text'
-										placeholder='Note'
-										value={editData.note}
-										onChange={(e) =>
-											setEditData({
-												...editData,
-												note: e.target.value,
-											})
-										}
-									/>
-									<div className='Edit-Buttons'>
+									<strong>Mood:</strong> {mood.mood}
+								</div>
+								<div>
+									<strong>Intensity:</strong> {mood.intensity}
+								</div>
+								<div>
+									<strong>Timestamp:</strong>{' '}
+									{new Date(mood.createdAt).toLocaleString()}
+								</div>
+								<div>
+									<strong>Note:</strong> {mood.note}
+								</div>
+								{editingId === mood._id ? (
+									<div>
+										<select
+											type='text'
+											placeholder='Mood'
+											value={editData.mood}
+											onChange={(e) =>
+												setEditData({
+													...editData,
+													mood: e.target.value,
+												})
+											}>
+											<option value=''>
+												Select Mood
+											</option>
+											<option value='happy'>Happy</option>
+											<option value='sad'>Sad</option>
+											<option value='angry'>Angry</option>
+											<option value='relaxed'>
+												Relaxed
+											</option>
+											<option value='stressed'>
+												Stressed
+											</option>
+										</select>
+										<input
+											type='text'
+											placeholder='Intensity'
+											value={editData.intensity}
+											onChange={(e) =>
+												setEditData({
+													...editData,
+													intensity: e.target.value,
+												})
+											}
+										/>
+										<textarea
+											type='text'
+											placeholder='Note'
+											value={editData.note}
+											onChange={(e) =>
+												setEditData({
+													...editData,
+													note: e.target.value,
+												})
+											}
+										/>
+										<div className='Edit-Buttons'>
+											<button
+												onClick={() =>
+													saveMood(mood._id)
+												}>
+												Save
+											</button>
+											<button
+												onClick={() =>
+													setEditingId(null)
+												}>
+												Cancel
+											</button>
+										</div>
+									</div>
+								) : (
+									<div>
 										<button
-											onClick={() => saveMood(mood._id)}>
-											Save
+											onClick={() => startEditing(mood)}>
+											Edit
 										</button>
 										<button
-											onClick={() => setEditingId(null)}>
-											Cancel
+											onClick={() =>
+												deleteMood(mood._id)
+											}>
+											Delete
 										</button>
 									</div>
-								</div>
-							) : (
-								<div>
-									<button onClick={() => startEditing(mood)}>
-										Edit
-									</button>
-									<button
-										onClick={() => deleteMood(mood._id)}>
-										Delete
-									</button>
-								</div>
-							)}
-						</li>
-					))}
-				</ul>
+								)}
+							</li>
+						))}
+					</ul>
+				</>
 			)}
 		</div>
 	)
